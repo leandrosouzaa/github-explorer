b9d84ac49286d44ce663be91184141e9854fb439
Leandro Souza <leandror.dev@gmail.com>
2020-04-20 19:09:46 -0300

    Routing done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Routes from './routes'
 
-function App() {
-  return (
-    <h1> Hello World </h1>
-  );
-}
+import { BrowserRouter } from "react-router-dom";
+
+const App: React.FC = () => (
+   <BrowserRouter>
+      <Routes />
+   </BrowserRouter>
+)
 
 export default App;
