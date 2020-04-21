f22a0fc2fcc618dfa753d487c7c241ab8f1a2fee
Leandro Souza <leandror.dev@gmail.com>
2020-04-21 13:26:11 -0300

    Detail screen done

--- frontend/src/pages/Repository/index.tsx
+++ frontend/src/pages/Repository/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
    repository: string;
@@ -8,7 +14,53 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
    const { params } = useRouteMatch<RepositoryParams>();
 
-   return <h1>Repository: {params.repository}</h1>;
+   return (
+      <>
+         <Header>
+            <img src={logoImg} alt="GitHub exlorer" />
+            <Link to="/">
+               <FiChevronLeft size={16} />
+               Voltar
+            </Link>
+         </Header>
+         <RepositoryInfo>
+            <header>
+               <img
+                  src="https://avatars0.githubusercontent.com/u/51727533?s=460&u=db66dcd045f5e01193f7e162c4bbc193886ae348&v=4"
+                  alt="leandrosouzaa"
+               />
+               <div>
+                  <strong>leandrosouzaa/habbap</strong>
+                  <p>descrição do repositório.</p>
+               </div>
+            </header>
+            <ul>
+               <li>
+                  <strong>10000</strong>
+                  <span>Stars</span>
+               </li>
+               <li>
+                  <strong>10000</strong>
+                  <span>Forks</span>
+               </li>
+               <li>
+                  <strong>10000</strong>
+                  <span>Issues abertas</span>
+               </li>
+            </ul>
+         </RepositoryInfo>
+         <Issues>
+            <Link key={1} to="/repository/">
+               <div>
+                  <strong>asdasdf</strong>
+                  <p>asdadsads</p>
+               </div>
+
+               <FiChevronRight size={20} />
+            </Link>
+         </Issues>
+      </>
+   );
 };
 
 export default Repository;
